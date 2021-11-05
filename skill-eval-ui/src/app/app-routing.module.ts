068ed9a5ee7ce1272ea0c7dce21948d45de32890
Nhan Do <nhan.ttdo@gmail.com>
2021-11-05 13:34:29 -0500

    Fix the routing and adding database sql files

--- skill-eval-ui/src/app/app-routing.module.ts
+++ skill-eval-ui/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './common/guard/auth.guard';
+import { AuthComponent } from './common/services/auth/auth.component';
+import { EmployeeDetailComponent } from './components/dashboard/employee/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './components/dashboard/employee/employee-edit/employee-edit.component';
+import { EmployeeStartComponent } from './components/dashboard/employee/employee-start/employee-start.component';
+import { EmployeeComponent } from './components/dashboard/employee/employee.component';
+import { EmployeesResolverService } from './components/dashboard/employee/employees-resolver.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/auth', pathMatch:'full'},
+  {path: 'employee', component: EmployeeComponent, canActivate:[AuthGuard],children:[
+    {path:'', component: EmployeeStartComponent},
+    {path:'new', component: EmployeeEditComponent},
+    {path:':id', component: EmployeeDetailComponent, resolve: [EmployeesResolverService]},
+    {path:':id/edit', component: EmployeeEditComponent, resolve: [EmployeesResolverService]},
+  ]},
+  {path: 'auth', component: AuthComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- skill-eval-ui/src/app/app.module.ts
+++ skill-eval-ui/src/app/app.module.ts
@@ -8,6 +8,14 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EditComponent } from './components/edit/edit.component';
 import { MypageComponent } from './components/mypage/mypage.component';
+import { SignupComponent } from './components/login/signup/signup.component';
+import { EmployeeEditComponent } from './components/dashboard/employee/employee-edit/employee-edit.component';
+import { EmployeeStartComponent } from './components/dashboard/employee/employee-start/employee-start.component';
+import { EmployeeDetailComponent } from './components/dashboard/employee/employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './components/dashboard/employee/employee-list/employee-list.component';
+import { EmployeeItemComponent } from './components/dashboard/employee/employee-list/employee-item/employee-item.component';
+import { AuthComponent } from './common/services/auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { MypageComponent } from './components/mypage/mypage.component';
     LoginComponent,
     DashboardComponent,
     EditComponent,
-    MypageComponent
+    MypageComponent,
+    SignupComponent,
+    EmployeeEditComponent,
+    EmployeeStartComponent,
+    EmployeeDetailComponent,
+    EmployeeListComponent,
+    EmployeeItemComponent,
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
