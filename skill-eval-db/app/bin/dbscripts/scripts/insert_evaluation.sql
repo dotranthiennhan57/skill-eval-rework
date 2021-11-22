2f7ca123eb53c95ec23b0682c5118c3d4f71a0f5
Nhan Do <nhan.ttdo@gmail.com>
2021-11-22 10:32:29 -0600

    Add, dummy data for evaluation

---
+++ skill-eval-db/app/bin/dbscripts/scripts/insert_evaluation.sql
@@ -0,0 +1,35 @@
+--Henry's Skill
+insert into evaluation (employee_id, skill_id, skill_rating) values (1, 1 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (1, 11 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (1, 12 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (1, 13 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (1, 14 , 10);
+
+--Ayo's Skill
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 2 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 21 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 22 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 23 , 10);
+
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 3 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 31 , 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 32, 10);
+insert into evaluation (employee_id, skill_id, skill_rating) values (2, 33 , 10);
+
+--Nhan's Skill
+insert into evaluation (employee_id, skill_id, skill_rating) values (3, 2 , 3);
+insert into evaluation (employee_id, skill_id, skill_rating) values (3, 21 , 3);
+insert into evaluation (employee_id, skill_id, skill_rating) values (3, 22, 3);
+insert into evaluation (employee_id, skill_id, skill_rating) values (3, 23 , 3);
+
+--Toan's Skill
+insert into evaluation (employee_id, skill_id, skill_rating) values (4, 3 , 5);
+insert into evaluation (employee_id, skill_id, skill_rating) values (4, 31 , 5);
+insert into evaluation (employee_id, skill_id, skill_rating) values (4, 32, 5);
+insert into evaluation (employee_id, skill_id, skill_rating) values (4, 33 , 5);
+
+--Michael's Skill
+insert into evaluation (employee_id, skill_id, skill_rating) values (5, 3 , 7);
+insert into evaluation (employee_id, skill_id, skill_rating) values (5, 31 , 7);
+insert into evaluation (employee_id, skill_id, skill_rating) values (5, 32 , 7);
+insert into evaluation (employee_id, skill_id, skill_rating) values (5, 33 , 7);
--- skill-eval-db/app/routes/employees.js
+++ skill-eval-db/app/routes/employees.js
@@ -4,21 +4,58 @@ const pool = require('../db');
 const router = Router();
 
 router.get('/', (request, response, next) => {
-    pool.query('SELECT * FROM Employees t1 LEFT OUTER JOIN Users t2 ON t1.user_id = t2.user_id LEFT OUTER JOIN position_list t3 ON t1.position_id = t3.position_id ORDER BY employee_id ASC', (err, res) => {
+    pool.query('SELECT * FROM Employees t1 LEFT OUTER JOIN Users t2 ON t1.user_id = t2.user_id LEFT OUTER JOIN position_list t3 ON t1.position_id = t3.position_id FULL OUTER JOIN evaluation t4 ON t1.employee_id = t4.employee_id ORDER BY t1.employee_id', (err, res) => {
       if (err) return next(err);
   
       response.json(res.rows);
     });
   });
   
-  router.get('/:id', (request, response, next) => {
-    const { id } = request.params;
-  
-    pool.query('SELECT * FROM Employees t1 LEFT OUTER JOIN Users t2 ON t1.user_id = t2.user_id LEFT OUTER JOIN position_list t3 ON t1.position_id = t3.position_id WHERE employee_id = $1', [id], (err, res) => {
-      if (err) return next(err);
-  
-      response.json(res.rows);
-    });
+router.get('/:id', (request, response, next) => {
+  const { id } = request.params;
+
+  pool.query('SELECT * FROM Employees t1 LEFT OUTER JOIN Users t2 ON t1.user_id = t2.user_id LEFT OUTER JOIN position_list t3 ON t1.position_id = t3.position_id FULL OUTER JOIN evaluation t4 ON t1.employee_id = t4.employee_id WHERE employee_id = $1', [id], (err, res) => {
+    if (err) return next(err);
+
+    response.json(res.rows);
   });
+});
+
+router.post('/:id', (request, response, next) => {
+  const { id } = request.params;
+  const { skill_id, skill_rating } = request.body;
+
+  pool.query(
+    'INSERT INTO Evaluation(skill_id, skill_rating) VALUES($1, $2)',
+    [id, skill_id, skill_rating],
+    (err, res) => {
+      if (err) return next(err);
+
+      response.redirect('/employees');
+    }
+  );
+});
+
+router.put('/:id', (request, response, next) => {
+  const { id } = request.params;
+  const keys = ['skill_id', 'skill_rating'];
+  const fields = [];
+
+  keys.forEach(key => {
+    if (request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) => {
+    pool.query(
+      `UPDATE Evaluation SET ${field}=($1) WHERE employee_id=($2)`,
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
+
+        if (index === fields.length - 1) response.redirect('/employees');
+      }
+    )
+  });
+});
 
   module.exports = router;
