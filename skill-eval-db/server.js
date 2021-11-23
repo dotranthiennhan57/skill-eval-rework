09101d3c73d1229356c06af6f56a91632925666b
Nhan Do <nhan.ttdo@gmail.com>
2021-11-22 22:25:32 -0600

    add server.js

--- skill-eval-db/server.js
+++ skill-eval-db/server.js
@@ -1,15 +1,33 @@
 const express =  require('express');
+const cors = require("cors");
 const routes = require('./app/routes');
 const pool = require('./app/db');
 
 const app = express();
 
+//This is connect to Angular
+var corsOptions = {
+  origin: "http://localhost:4581"
+}
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
 app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// simple route
 app.use('/', routes);
 
 app.use((err, req, res, next) => {
   res.json(err);
 });
+
+//routes
+// require('./app/routes/employees')(app);
+
+
 
 module.exports = app;
