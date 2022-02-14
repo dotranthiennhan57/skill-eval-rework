48a6462b251c944cb62cb870d8b94af6cf5737c6
Nhan Do <nhan.ttdo@gmail.com>
2022-02-14 13:55:53 -0600

    add skills to db scripts

--- skill-eval-db/postgres/dbscripts/scripts/insert_skills.sql
+++ skill-eval-db/postgres/dbscripts/scripts/insert_skills.sql
@@ -22,6 +22,25 @@ insert into Skills(skill_id,skill_name,subskillof) values(9,'Project Manager',nu
 insert into Skills(skill_id,skill_name,subskillof) values(91,'Time management',9);
 insert into Skills(skill_id,skill_name,subskillof) values(92,'Negotiation',9);
 
+insert into Skills(skill_id,skill_name,subskillof) values(4,'Java',null);
+--Subskills of Java
+insert into Skills(skill_id,skill_name,subskillof) values(41,'Spring Boot',4);
+insert into Skills(skill_id,skill_name,subskillof) values(42,'Strut',4);
+
+insert into Skills(skill_id,skill_name,subskillof) values(5,'C#',null);
+--Subskills of Java
+insert into Skills(skill_id,skill_name,subskillof) values(51,'.NET',5);
+insert into Skills(skill_id,skill_name,subskillof) values(52,'ASP.NET',5);
+
+insert into Skills(skill_id,skill_name,subskillof) values(6,'SQL',null);
+--Subskills of Java
+insert into Skills(skill_id,skill_name,subskillof) values(61,'Postgres',6);
+insert into Skills(skill_id,skill_name,subskillof) values(62,'MySQL',6);
+insert into Skills(skill_id,skill_name,subskillof) values(62,'BigQuery',6);
+
+
+
+
 
 
 
